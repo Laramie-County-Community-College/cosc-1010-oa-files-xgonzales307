f77edd01f608e6c7f5bb5e6b1a387f8346125a7f
xgonzales307 <xgonzales307@gmail.com>
2026-04-18 19:12:13 +0000

    Changes

--- 03_Files_Predict_1.py
+++ 03_Files_Predict_1.py
@@ -11,7 +11,7 @@ Activity: Predict the Output
 """
 
 # What will be the final text inside story.txt?
-prediction = "" # Example: "The end."
+prediction = "The sun was shining."
 
 # The 'w' mode stands for 'write'. It will overwrite the file.
 file = open("story.txt", "w")
@@ -21,3 +21,4 @@ file.write("The sun was shining.")
 file.close()
 
 # If you were to open and read "story.txt" now, what would you see?
+# You would see: "The sun was shining."
